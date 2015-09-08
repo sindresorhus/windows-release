9abdc22b0430cb5e5334cde5726c243d55fc319a
Sindre Sorhus <sindresorhus@gmail.com>
2015-09-08 14:30:34 +0700

    bump AVA

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ var test = require('ava');
 var winRelease = require('./');
 
 test(function (t) {
-	t.assert(winRelease('5.1.2600') === 'XP');
-	t.assert(winRelease('10.0.10240') === '10');
+	t.is(winRelease('5.1.2600'), 'XP');
+	t.is(winRelease('10.0.10240'), '10');
 	t.end();
 });
