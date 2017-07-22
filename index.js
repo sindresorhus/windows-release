69a622d3a275dff1f55658738f5e00baabc7eddb
silverwind <me@silverwind.io>
2017-07-22 19:44:53 +0200

    Add Windows Server detection, require node 4
    - Detect Windows Server 2008, 2012 and 2016 via a `wmic` call
    - Remove workaround for io.js 3 and older node versions
    - ES2015ify
    
    Fixes: https://github.com/sindresorhus/win-release/issues/5

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-'use strict';
-var os = require('os');
-var semver = require('semver');
+/* eslint-disable quote-props */
 
-var nameMap = {
+'use strict';
+const os = require('os');
+const execa = require('execa');
+
+// Reference: https://www.gaijin.at/en/lstwinver.php
+const nameMap = {
 	'10.0': '10',
 	'6.3': '8.1',
 	'6.2': '8',
 	'6.1': '7',
 	'6.0': 'Vista',
+	'5.2': 'Server 2003',
 	'5.1': 'XP',
 	'5.0': '2000',
 	'4.9': 'ME',
@@ -16,20 +20,24 @@ var nameMap = {
 };
 
 module.exports = function (release) {
-	var verRe = /\d+\.\d+/;
-	var version = verRe.exec(release || os.release());
+	const rel = release || os.release();
+	const verRe = /\d+\.\d+/;
+	const version = verRe.exec(rel);
 
-	// workaround for Windows 10 on node < 3.1.0
-	if (!release && process.platform === 'win32' &&
-		semver.satisfies(process.version, '>=0.12.0 <3.1.0')) {
-		try {
-			version = verRe.exec(String(require('child_process').execSync('ver.exe', {timeout: 2000})));
-		} catch (err) {}
-	}
-
-	if (release && !version) {
+	if (rel && !version) {
 		throw new Error('`release` argument doesn\'t match `n.n`');
 	}
 
-	return nameMap[(version || [])[0]];
+	const ver = (version || [])[0];
+
+	// Server 2008, 2012 and 2016 version are ambiguous with desktop release versions
+	if (!release && ['6.1', '6.2', '6.3', '10.0'].indexOf(ver) !== -1) {
+		const child = execa.sync('wmic', ['os', 'get', 'Caption']);
+		const year = ((child.stdout || '').match(/2008|2012|2016/) || [])[0];
+		if (year) {
+			return `Server ${year}`;
+		}
+	}
+
+	return nameMap[ver];
 };
